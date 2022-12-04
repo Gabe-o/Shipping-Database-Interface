b38ec12ae3af2403b3662093570ec485309207eb
Gabriel Olivotto <golivott@uwo.ca>
2022-12-04 18:19:06 -0500

    Made menu for adding ships to a route

--- src/backend/routes/complexRouter.js
+++ src/backend/routes/complexRouter.js
@@ -3,10 +3,21 @@ const db = require('../DBConnect.js');
 
 const complexRouter = express.Router();
 
-complexRouter.get("routeDetails", (req, res) => {
+complexRouter.get("/routeDetails", (req, res) => {
 
     db.query(
-        `SELECT 
+        `SELECT
+        routeDetails.routeNo,
+        routeDetails.startingPortNo,
+        routeDetails.endingPortNo,
+        routeDetails.distance,
+        routeDetails.numShipments,
+        routeDetails.currentWeight,
+        IFNULL(SUM(ships.maxCargoWeight),0) AS cargoCapacity,
+        COUNT(ships.shipID) AS numShips
+        FROM ships
+        RIGHT JOIN
+        (SELECT 
         routes.routeNo,
         routes.startingPortNo,
         routes.endingPortNo,
@@ -21,18 +32,51 @@ complexRouter.get("routeDetails", (req, res) => {
         INNER JOIN routes
         ON shipments.routeNo=routes.routeNo 
         GROUP BY routes.routeNo
-        ORDER BY routeNo ASC;`,
+        ORDER BY routeNo ASC) 
+        AS routeDetails
+        ON routeDetails.routeNo=ships.routeNo
+        GROUP BY routeNo;`,
         (err, data) => {
-            if (err != null) {
-                res.status(500).json("Error getting route details data!")
+            if (err) {
+                res.status(500).json("Error getting route details data!");
             }
             else if (data.length === 0) {
-                res.status(404).json("route details has no data!");
+                res.status(404).json("Route details has no data!");
             }
             else {
                 res.status(200).json(data);
             }
         });
 });
+
+complexRouter.get("/shipsOnRoute/:routeNo", (req, res) => {
+
+    db.query(`SELECT * FROM ships WHERE routeNo=?;`, [req.params.routeNo], (err, data) => {
+        if (err) {
+            res.status(500).json(`Error getting ships on route ${req.params.routeNo} data!`);
+        }
+        else if (data.length === 0) {
+            res.status(404).json(`Ships on route ${req.params.routeNo} has no data!`);
+        }
+        else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+complexRouter.get("/shipsCanDoRoute/:routeNo", (req, res) => {
+
+    db.query(`SELECT * FROM ships WHERE maxRange >= (SELECT distance FROM routes WHERE routeNo=1) AND routeNo IS NULL;`, [req.params.routeNo], (err, data) => {
+        if (err) {
+            res.status(500).json(`Error getting ships that can do route ${req.params.routeNo} data!`);
+        }
+        else if (data.length === 0) {
+            res.status(404).json(`Ships that can do route ${req.params.routeNo} has no data!`);
+        }
+        else {
+            res.status(200).json(data);
+        }
+    });
+})
 
 module.exports = complexRouter;
