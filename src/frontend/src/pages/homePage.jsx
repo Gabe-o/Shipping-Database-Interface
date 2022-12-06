0324957d38ebe7cb6a439518b8c55ab355db4fc2
NicoM-7 <nic7moniz@gmail.com>
2022-12-05 23:27:07 -0500

    Added a message in the home page

--- src/frontend/src/pages/homePage.jsx
+++ src/frontend/src/pages/homePage.jsx
@@ -7,7 +7,7 @@ function HomePage() {
         <div className='defaultMargin'>
             <h1>Database Assignment 4</h1>
             <h2>Made By Gabe Olivotto, Nico Moniz, Thomas Aiello, Brennan Cowley Adam and Ayush Shawarma</h2>
-            <p>Fill in later</p>
+            <p>This web app allows you to interact with every table in our database as well as 5 unique complex functionalities you can perform with the tables!</p>
         </div>
     );
 }
